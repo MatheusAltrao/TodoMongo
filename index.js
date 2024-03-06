0ee641807389b21441feaeacaa44b64b6492dd06
Matheus Altrao <matheusaltrao2@gmail.com>
2024-03-06 15:54:34 -0400

    feat: add route to delete task

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv')
 
-
 const connectToDatabase = require('./src/database/mongoose.database')
 const TaskModel = require('./src/models/task.model')
 
@@ -34,5 +33,26 @@ app.post('/tasks', async (req, res) => {
         res.status(500).send(error.message)
     }
 })
+
+//vamos deletar uma tarefa e precisamos passar o id da tarefa no parametro da url
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const taskId = req.params.id
+        const taskToDelete = await TaskModel.findById(taskId)
+
+        if (!taskToDelete) {
+            return req.status(500).send('Essa tarefa não foi encontrada')
+        }
+
+        const deletedTask = await TaskModel.findByIdAndDelete(taskId)
+
+        res.status(200).send(deletedTask)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
+
 
 app.listen(8000, () => console.log('Listening on port 8000'))
