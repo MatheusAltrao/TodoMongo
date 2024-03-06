6593aed950fe18faf676e39d2e7bf0ef24f1d8eb
Matheus Altrao <matheusaltrao2@gmail.com>
2024-03-06 13:37:46 -0400

    feat: add task model

---
+++ src/models/task.model.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose')
+
+
+//criando a entidade task e tambm o schema passando os tipos e os nomes dos campos
+const TaskSchema = Schema({
+    description: {
+        type: String,
+        required: true,
+    },
+    isCompleted: {
+        type: boolean,
+        default: false
+    }
+})
+
+const TaskModel = model('Task', TaskSchema)
+
+module.exports = TaskModel
+
