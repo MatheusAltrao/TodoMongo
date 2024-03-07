96761718cb9deafe473bc5fc1896acdf543972f2
Matheus Altrao <matheusaltrao2@gmail.com>
2024-03-07 11:20:20 -0400

    feat: add route to update a task

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv')
 const connectToDatabase = require('./src/database/mongoose.database')
 const TaskModel = require('./src/models/task.model')
 
+
 dotenv.config()
 const app = express()
 //falando pro express que vamos receber json na body da requisicao
@@ -52,8 +53,38 @@ app.post('/tasks', async (req, res) => {
     }
 })
 
-//vamos deletar uma tarefa e precisamos passar o id da tarefa no parametro da url
 
+//atualizando o iscompleted da task e não deixando atualizar a descrição
+app.patch('/tasks/:id', async (req, res) => {
+    try {
+        const taskId = req.params.id
+        const tasksData = req.body
+
+        const taskToUpdate = await TaskModel.findById(taskId)
+
+        const allowedUpdates = ['isCompleted'] // campo q pode ser atualizado
+        const requestUpdates = Object.keys(tasksData) //pegando os campos q estão vindo do body em json
+
+        //para cada campo do body verificar se os campos estão incluidos no tasktoupdate pode receber o novo valor
+        for (update of requestUpdates) {
+            if (allowedUpdates.includes(update)) {
+                //seria o mesmo que tasktoUpadate.isCompleted = ao valor recebido vindo do update
+                taskToUpdate[update] = tasksData[update]
+            } else {
+                return res.status(500).send('Um ou mais campos inseridos não são editaveis')
+            }
+        }
+
+        await taskToUpdate.save()
+
+
+        return res.status(200).send(taskToUpdate)
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+})
+
+//vamos deletar uma tarefa e precisamos passar o id da tarefa no parametro da url
 app.delete('/tasks/:id', async (req, res) => {
     try {
         const taskId = req.params.id
