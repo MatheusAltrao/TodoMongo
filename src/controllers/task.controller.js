4788354968de99957a4bc4125826bfbfee7e779d
Matheus Altrao <matheusaltrao2@gmail.com>
2024-03-07 22:19:13 -0400

    feat: add update and delete router to controller

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -30,6 +30,62 @@ class TaskController {
             this.res.status(500).send(error.message)
         }
     }
+
+    async createTask() {
+        try {
+            const newTask = new TaskModel(this.req.body)
+            await newTask.save()
+            this.res.status(201).send(newTask)
+        } catch (error) {
+            this.res.status(500).send(error.message)
+        }
+    }
+
+    async updateTask() {
+        try {
+            const taskId = this.req.params.id
+            const tasksData = this.req.body
+
+            const taskToUpdate = await TaskModel.findById(taskId)
+
+            const allowedUpdates = ['isCompleted'] // campo q pode ser atualizado
+            const requestUpdates = Object.keys(tasksData) //pegando os campos q estão vindo do body em json
+
+            //para cada campo do body verificar se os campos estão incluidos no tasktoupdate pode receber o novo valor
+            for (const update of requestUpdates) {
+                if (allowedUpdates.includes(update)) {
+                    //seria o mesmo que tasktoUpadate.isCompleted = ao valor recebido vindo do update
+                    taskToUpdate[update] = tasksData[update]
+                } else {
+                    return this.res.status(500).send('Um ou mais campos inseridos não são editaveis')
+                }
+            }
+
+            await taskToUpdate.save()
+
+
+            return this.res.status(200).send(taskToUpdate)
+        } catch (error) {
+            return this.res.status(500).send(error.message)
+        }
+    }
+
+    async deleteTask() {
+        try {
+            const taskId = this.req.params.id
+            const taskToDelete = await TaskModel.findById(taskId)
+
+            if (!taskToDelete) {
+                return this.req.status(404).send('Essa tarefa não foi encontrada')
+            }
+
+            const deletedTask = await TaskModel.findByIdAndDelete(taskId)
+
+            this.res.status(200).send(deletedTask)
+        } catch (error) {
+            this.res.status(500).send(error.message)
+        }
+    }
 }
 
 module.exports = TaskController
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -18,61 +18,17 @@ router.get('/:id', async (req, res) => {
 
 //vamos usar a request para enviar os dados para o banco e salvando
 router.post('', async (req, res) => {
-    try {
-        const newTask = new TaskModel(req.body)
-        await newTask.save()
-        res.status(201).send(newTask)
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
+    return new TaskController(req, res).createTask()
 })
 
 //atualizando o iscompleted da task e não deixando atualizar a descrição
 router.patch('/:id', async (req, res) => {
-    try {
-        const taskId = req.params.id
-        const tasksData = req.body
-
-        const taskToUpdate = await TaskModel.findById(taskId)
-
-        const allowedUpdates = ['isCompleted'] // campo q pode ser atualizado
-        const requestUpdates = Object.keys(tasksData) //pegando os campos q estão vindo do body em json
-
-        //para cada campo do body verificar se os campos estão incluidos no tasktoupdate pode receber o novo valor
-        for (update of requestUpdates) {
-            if (allowedUpdates.includes(update)) {
-                //seria o mesmo que tasktoUpadate.isCompleted = ao valor recebido vindo do update
-                taskToUpdate[update] = tasksData[update]
-            } else {
-                return res.status(500).send('Um ou mais campos inseridos não são editaveis')
-            }
-        }
-
-        await taskToUpdate.save()
-
-
-        return res.status(200).send(taskToUpdate)
-    } catch (error) {
-        return res.status(500).send(error.message)
-    }
+    return new TaskController(req, res).updateTask()
 })
 
 //vamos deletar uma tarefa e precisamos passar o id da tarefa no parametro da url
 router.delete('/:id', async (req, res) => {
-    try {
-        const taskId = req.params.id
-        const taskToDelete = await TaskModel.findById(taskId)
-
-        if (!taskToDelete) {
-            return req.status(404).send('Essa tarefa não foi encontrada')
-        }
-
-        const deletedTask = await TaskModel.findByIdAndDelete(taskId)
-
-        res.status(200).send(deletedTask)
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
+    return new TaskController(req, res).deleteTask()
 })
 
 
